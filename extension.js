90291523cc7ad6ec2afd5c47ab355b4097d6c831
Kevin Kenger <kevinkenger@gmail.com>
2021-02-15 15:47:54 -0700

    add readme & update data

--- extension.js
+++ extension.js
@@ -1,4 +1,40 @@
-{
-    "type": "getUserScreen",
-    "sources": ["screen", "window", "tab"]
+chrome.runtime.onMessageExternal.addListener((message, sender, sendResponse) => {
+    switch (message && message.type) {
+        // Our web app sent us a "getUserScreen" request.
+        case 'getUserScreen':
+            handleGetUserScreenRequest(message.sources, sender.tab, sendResponse);
+            break;
+
+        // Our web app sent us a request we don't recognize.
+        default:
+            handleUnrecognizedRequest(sendResponse);
+            break;
+    }
+
+    return true;
+});
+
+function handleGetUserScreenRequest(sources, tab, sendResponse) {
+    chrome.desktopCapture.chooseDesktopMedia(sources, tab, streamId => {
+        // The user canceled our request.
+        if (!streamId) {
+            sendResponse({
+                type: 'error',
+                message: 'Failed to get stream ID'
+            });
+        }
+
+        // The user accepted our request.
+        sendResponse({
+            type: 'success',
+            streamId: streamId
+        });
+    });
+}
+
+function handleUnrecognizedRequest(sendResponse) {
+    sendResponse({
+        type: 'error',
+        message: 'Unrecognized request'
+    });
 }
